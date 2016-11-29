9487f2636a64bd735c8b2ea47a150495bb4c925f
mostofamonsur <mostofa.monsur@rootnext.com>
2016-11-29 17:10:44 +0600

    mario game initialized

---
+++ android/src/com/rootnext/super/mario/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rootnext.super.mario.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rootnext.super.mario.SuperMario;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SuperMario(), config);
+	}
+}
---
+++ core/src/com/rootnext/supermario/SuperMario.java
@@ -0,0 +1,26 @@
+package com.rootnext.supermario;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class SuperMario extends Game {
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+//		batch.draw(img, 0, 0);
+		batch.end();
+	}
+}
