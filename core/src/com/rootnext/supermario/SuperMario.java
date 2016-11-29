88d5b64a57e88c99df38a4cb08e14ed33b048261
mostofamonsur <mostofa.monsur@rootnext.com>
2016-11-29 17:35:09 +0600

    part 3 game screen added

--- core/src/com/rootnext/supermario/SuperMario.java
+++ core/src/com/rootnext/supermario/SuperMario.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.rootnext.supermario.screens.PlayScreen;
 
 public class SuperMario extends Game {
 	public SpriteBatch batch;
@@ -13,14 +14,11 @@ public class SuperMario extends Game {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-//		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 }
